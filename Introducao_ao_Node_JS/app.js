72ab8a1da11242b6ede594999bb235cbfc66658c
edsonxrego <edsonxrego@gmail.com>
2022-02-10 17:51:27 -0300

    Started project

---
+++ Introducao_ao_Node_JS/app.js
@@ -0,0 +1,38 @@
+//console.log("Olá, Node.");
+
+var express = require('express');
+var app = express();
+
+app.get('/contato',function(req,res){
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset="utf-8">
+        <title>Introdução ao Node JS</title>
+      </head>
+      <body>
+        <h1>Página de Contato</h1>
+      </body>
+    </html>
+    `);
+});
+
+app.get('/',function(req,res){
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset="utf-8">
+        <title>Introdução ao Node JS</title>
+      </head>
+      <body>
+        <h1>Página Home</h1>
+      </body>
+    </html>
+    `);
+});
+
+app.listen(8000,function(){
+  console.log("localhost:8000");
+});
